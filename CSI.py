d412f7df10f3f41ff339652b1b88d2b0eeaff52d
Sina Haghighi <sina.liana.2001@gmail.com>
2024-06-05 13:28:14 -0700

    csi camera works wit existing algorithm

--- CSI.py
+++ CSI.py
@@ -8,11 +8,18 @@ serial_port = '/dev/ttyTHS1'
 baud_rate = 9600
 ser = serial.Serial(serial_port, baud_rate, timeout=1)
 
-def gstreamer_pipeline(sensor_id=0, capture_width=640, capture_height=480, framerate=30, flip_method=0):
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=1920,
+    display_height=1080,
+    framerate=30,
+    flip_method=0,
+):
     return (
         "nvarguscamerasrc sensor-id=%d ! "
-        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
         "nvvidconv flip-method=%d ! "
         "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
         "videoconvert ! "
@@ -23,10 +30,11 @@ def gstreamer_pipeline(sensor_id=0, capture_width=640, capture_height=480, frame
             capture_height,
             framerate,
             flip_method,
-            capture_width,
-            capture_height,
+            display_width,
+            display_height,
         )
     )
+
 
 pipeline0 = gstreamer_pipeline(sensor_id=0)
 pipeline1 = gstreamer_pipeline(sensor_id=1)
